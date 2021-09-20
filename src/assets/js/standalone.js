3d4187c46238f383bfb76603fcc1293f06197bbc
Fredrik Arnstad <fredrik.arnstad@icloud.com>
2021-09-20 11:16:04 +0200

    The feed needs to be opened in the correct app

--- src/assets/js/standalone.js
+++ src/assets/js/standalone.js
@@ -1,5 +1,9 @@
-const feed = document.querySelector('.feed')
+// const feed = document.querySelector('.feed')
 
-if (navigator.standalone === true) {
-  feed.remove()
+// if (navigator.standalone === true) {
+//   feed.remove()
+// }
+
+const openRSS = feed => {
+  window.open(`${window.origin}/${feed}`)
 }
